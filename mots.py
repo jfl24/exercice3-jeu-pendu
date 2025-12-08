98fc500cb214a126fe9db0160ba6e331a50f08db
Jean-Francois Pierre <e0429114@cmaisonneuve.qc.ca>
2025-12-08 17:39:50 -0500

    Fonction mots

---
+++ mots.py
@@ -0,0 +1,11 @@
+import random
+
+mots = ["python", "programmation", "github", "collaboration", "projet", "automne"]
+def choisir_mot():
+    secret = random.randint(1, len(mots) - 1)
+    mot_secret = mots[secret]
+    return mot_secret
+
+
+
+
