cd611bd05fd04276816aad1743d94c4138274e7d
Jean-Francois Pierre <e0429114@cmaisonneuve.qc.ca>
2025-12-08 17:47:14 -0500

    Branche Main

--- mots.py
+++ mots.py
@@ -1,10 +1,4 @@
-import random
 
-mots = ["python", "programmation", "github", "collaboration", "projet", "automne"]
-def choisir_mot():
-    secret = random.randint(1, len(mots) - 1)
-    mot_secret = mots[secret]
-    return mot_secret
 
 
 
