979d725f530f6344c5d927d97b912e86fc73875c
Nerkesly Duvers <nerkeslyduvers@MacBook-Air-de-Nerkesly.local>
2025-12-18 15:58:06 -0500

    ajoutDeMain

---
+++ main.py
@@ -0,0 +1,37 @@
+
+from mots import choisir_mot       
+from affichage import afficher_pendu  
+from jeu import jouer_tour         
+
+def main():
+    mot_secret = choisir_mot()
+    lettres_trouvees = set()
+    erreurs = 0
+    max_erreurs = 6
+
+    print("=== JEU DU PENDU ===")
+
+    while erreurs < max_erreurs:
+        afficher_pendu(erreurs)
+
+        affichage = "".join(
+            lettre if lettre in lettres_trouvees else "_"
+            for lettre in mot_secret
+        )
+        print("Mot :", affichage)
+
+        if "_" not in affichage:
+            print("Bravo, tu as gagné !")
+            return
+
+        lettre = input("Choisis une lettre : ").lower()
+
+        erreur = jouer_tour(mot_secret, lettres_trouvees, lettre)
+        if erreur:
+            erreurs += 1
+
+    afficher_pendu(erreurs)
+    print("Perdu ! Le mot était :", mot_secret)
+
+if __name__ == "__main__":
+    main()
